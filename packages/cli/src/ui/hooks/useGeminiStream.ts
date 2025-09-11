9670dfd83c408e83e1e22f2767523551333bd657
Gal Zahavi <38544478+galz10@users.noreply.github.com>
2025-09-10 20:29:12 -0700

    WIP2

--- packages/cli/src/ui/hooks/useGeminiStream.ts
+++ packages/cli/src/ui/hooks/useGeminiStream.ts
@@ -785,7 +785,7 @@ export const useGeminiStream = (
           const recursionDepth = options?.recursionDepth ?? 0;
           if (recursionDepth < 2) {
             // To prevent infinite loops
-            await submitQuery(finalizationOutput.recursivePayload.query, {
+            await submitQuery(finalizationOutput.recursivePayload['query'], {
               isContinuation: true,
               recursionDepth: recursionDepth + 1,
             });
